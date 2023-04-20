0a269846dbcf027915134062b8b2afe5616a49c2
Kaushik Rathod <consultant_varun.shah@thedatateam.com>
2023-04-20 19:33:58 +0530

    Added swagger

--- apps/my-nest-app/src/main.ts
+++ apps/my-nest-app/src/main.ts
@@ -3,16 +3,47 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  
+  // This line of code for the payload validation.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors();
+  
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORTTWO || 3333;96
+
+  // This line of code for the swagger builder.
+  const config = new DocumentBuilder()
+    .setTitle('Store Hub')
+    .setDescription('E-Commerce Project')
+    .setVersion('1.0')
+    .addTag('Store Hub')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+
+  // This line of code for create swagger document.
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+  
+  const port = process.env.PORTTWO || 3333;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
